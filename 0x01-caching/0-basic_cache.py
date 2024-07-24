3f0bbba0940f5cc92062838fd9376a8b1eafa2d1
Onyoniken <onyonikenn@gmail.com>
2024-07-24 11:02:54 +0300

    0x01-caching

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,22 +1,30 @@
 #!/usr/bin/env python3
-"""Create a class BasicCache that inherits from BaseCaching and is a caching system:
+"""Create a class BasicCache that inherits from
+BaseCaching and is a caching system:
 
-    You must use self.cache_data - dictionary from the parent class BaseCaching
-    This caching system doesn’t have limit
-    def put(self, key, item):
-    Must assign to the dictionary self.cache_data the item value for the key key.
-    If key or item is None, this method should not do anything.
-    def get(self, key):
-    Must return the value in self.cache_data linked to key.
-    If key is None or if the key doesn’t exist in self.cache_data, return None.
-    """
+You must use self.cache_data - dictionary from
+the parent class BaseCaching
+This caching system doesn’t have limit
+def put(self, key, item):
+Must assign to the dictionary self.cache_data the
+item value for the key key.
+If key or item is None, this method should not do
+anything.
+def get(self, key):
+Must return the value in self.cache_data linked
+to key.
+If key is None or if the key doesn’t exist in
+self.cache_data, return None.
+"""
 
 
 BaseCaching = __import__('base_caching').BaseCaching
 
+
 class BasicCache(BaseCaching):
     """_summary_
     """
+
     def __init__(self):
         """_summary_
         """
@@ -24,6 +32,7 @@ class BasicCache(BaseCaching):
 
     def put(self, key, item):
         """_summary_
+
         Args:
                 key (_type_): _description_
                 item (_type_): _description_
--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,20 +1,31 @@
 #!/usr/bin/env python3
-"""Create a class FIFOCache that inherits from BaseCaching and is a caching system:
+"""Create a class FIFOCache that inherits from
+BaseCaching and is a caching system:
 
-    You must use self.cache_data - dictionary from the parent class BaseCaching
-    You can overload def __init__(self): but don’t forget to call the parent init: super().__init__()
-    def put(self, key, item):
-    Must assign to the dictionary self.cache_data the item value for the key key.
-    If key or item is None, this method should not do anything.
-    If the number of items in self.cache_data is higher that BaseCaching.MAX_ITEMS:
-    you must discard the first item put in cache (FIFO algorithm)
-    you must print DISCARD: with the key discarded and following by a new line
-    def get(self, key):
-    Must return the value in self.cache_data linked to key.
-    If key is None or if the key doesn’t exist in self.cache_data, return None.
+You must use self.cache_data - dictionary from
+the parent class BaseCaching
+You can overload def __init__(self): but don’t
+forget to call the parent init: super().__init__()
+def put(self, key, item):
+Must assign to the dictionary self.cache_data
+the item value for the key key.
+If key or item is None, this method should not
+do anything.
+If the number of items in self.cache_data is
+higher that BaseCaching.MAX_ITEMS:
+you must discard the first item put in cache
+(FIFO algorithm)
+you must print DISCARD: with the key discarded and
+following by a new line
+def get(self, key):
+Must return the value in self.cache_data linked to key.
+If key is None or if the key doesn’t exist in
+self.cache_data, return None.
 """
 
+
 BaseCaching = __import__('base_caching').BaseCaching
+
 
 class FIFOCache(BaseCaching):
     """_summary_
@@ -29,8 +40,8 @@ class FIFOCache(BaseCaching):
         """_summary_
 
         Args:
-                    key (_type_): _description_
-                    item (_type_): _description_
+                        key (_type_): _description_
+                        item (_type_): _description_
         """
         if key is None or item is None:
             pass
@@ -47,7 +58,7 @@ class FIFOCache(BaseCaching):
         """return the value in self.cache_data linked to key
 
         Args:
-                    key (_type_): _description_
+                        key (_type_): _description_
         """
         if key is None or key not in self.cache_data.keys():
             return None
